4b9c7ff6cc603940e320dab3aa8d402baf363526
kimhs982 <kimhyungsuk982@gmail.com>
2022-03-09 21:26:48 +0900

    1.3.1 연습문제1

---
+++ Data Structures and Algorithm practice/p1_3_1_main.cpp
@@ -0,0 +1,40 @@
+#include "p1_3_1.h"
+
+int main()
+{
+	int nStudents;
+	std::cout << "1반 학생 수를 입력하세요: ";
+	std::cin >> nStudents;
+
+	dynamic_array<student> class1(nStudents);
+	for (int i = 0; i < nStudents; i++)
+	{
+		std::string name;
+		int standard;
+		std::cout << i + 1 << "번째 학생 이름과 나이를 입력하세요: ";
+		std::cin >> name >> standard;
+		class1[i] = student{ name, standard };
+	}
+
+	// 배열보다 큰 인덱스의 학생에 접근
+	try
+	{
+		//class1[nStudents] = student{ "John",8 };
+
+		class1.at(nStudents) = student{ "John",8 };
+	}
+	catch (...)
+	{
+		std::cout << "예외 발생!" << std::endl;
+	}
+
+	// 깊은 복사
+	auto class2 = class1;
+	std::cout << "1번을 복사하여 2번 생성: " << class2.to_string() << std::endl;
+
+	// 두 학급을 합쳐서 새로운 큰 학급을 생성
+	auto class3 = class1 + class2;
+	std::cout << "1반과 2반을 합쳐 3반 생성: " << class3.to_string() << std::endl;
+
+	return 0;
+}
