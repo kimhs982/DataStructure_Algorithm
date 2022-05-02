fe2560c0e2a2188d29e0b865215e523c666d580d
kimhs982 <kimhyungsuk982@gmail.com>
2022-05-02 16:55:04 +0900

    과제1(5월 2일)-주석

--- AlgorithmAssignment1_12181763/BoyerMoore(main).cpp
+++ AlgorithmAssignment1_12181763/BoyerMoore(main).cpp
@@ -27,23 +27,23 @@ int main() {
 	else
 		cout << "파일을 찾을 수 없습니다!\n";
 
-	int rep = 1;
+	int rep = 1;		// 반복 여부를 저장
 	while (rep) {
 		cout << "검색할 패턴을 입력하세요 : ";
-		getline(cin, pattern);
+		getline(cin, pattern);		// 공백(' ')을 포함하여 검색할 문자열을 입력받음
 
-		int pos, previous = 0;
+		int pos, previous = 0;		// pattern이 발생한 위치를 저장하는 pos와 이전에 검색한 pattern의 위치를 저장하는 previous
 		while (1) {
 			pos = BMmatch(text, pattern, previous);
-			if (pos != -1)
+			if (pos != -1)		// text에서 pattern과 일치하는 부분을 발견한 경우
 				cout << "패턴이 발생한 위치 : " << pos << '\n';
 			else
 				break;
-			previous = pos + pattern.length();
+			previous = pos + pattern.length();		// pattern이 발생한 위치에 pattern의 길이를 더해 previous에 저장
 		}
 		cout << "[다시 검색(1), 종료(0)] : ";
 		cin >> rep;
-		cin.ignore();
+		cin.ignore();		// cin을 통해 rep를 입력 받으면 마지막 개행문자('\n')가 버퍼에 남아 while() 문을 반복할 경우 위의 getline()에 개행문자('\n')가 자동으로 입력됨 -> 버퍼에 남아있는 개행문자('\n')를 지워줌
 	}
 	cout << "탐색 종료\n";
 
--- AlgorithmAssignment1_12181763/BoyerMoore.h
+++ AlgorithmAssignment1_12181763/BoyerMoore.h
@@ -12,31 +12,35 @@ using namespace std;
 vector<int> buildLastFunction(const string& pattern) {
 	const int ASCII_N = 128;		// 총 128개의 ASCII 코드값
 	vector<int> last(ASCII_N, -1);		// 128개의 공간을 -1로 초기화
-	for (int i = 0; i < pattern.size(); i++)
+	for (int i = 0; i < pattern.size(); i++)		//  pattern에 있는 문자들은 다른 값을 할당
 		last[pattern[i]] = i;
 	return last;
 }
 
+// 보이어-무어 알고리즘의 간단화 버전(불일치 문자, mismatched character만 사용)
+// text에서 pattern에 해당하는 문자열을 찾은 경우 해당 문자열 시작 부분의 index를 반환, 찾지 못한 경우 -1을 반환
+// prev로 검색을 시작할 위치를 받음
 int BMmatch(const string& text, const string& pattern, int prev) {
-	vector<int> last = buildLastFunction(pattern);
-	int n = text.size();
-	int m = pattern.size();
-	int i = prev + m - 1;
-	if (i > n - 1)
+	vector<int> last = buildLastFunction(pattern);		// 해당 pattern에 대한 last 배열을 만듦
+	int n = text.size();		// text의 길이
+	int m = pattern.size();		// pattern의 길이
+
+	int i = prev + m - 1;		// text 검색 index
+	if (i > n - 1)		// 검색하려는 pattern이 text보다 긴 경우
 		return -1;
-	int j = m - 1;
+	int j = m - 1;		// pattern 검색 index
 	do {
-		if (pattern[j] == text[i])
-			if (j == 0) return i;
-			else {
+		if (pattern[j] == text[i])		// text[i]와 pattern[j]가 일치하는 경우
+			if (j == 0) return i;		// pattern의 모든 부분이 일치할 경우
+			else {		// 패턴을 거꾸로 비교
 				i--;
 				j--;
 			}
 		else {
-			i = i + m - min(j, 1 + last[text[i]]);
-			j = m - 1;
+			i = i + m - min(j, 1 + last[text[i]]);		// 이전 위치(i) + pattern의 길이 + j(pattern 검색 index)와 1 + last[text[i]](pattern과 불일치가 일어난 text[i]의 last 배열에 해당하는 값) 중에 최소값
+			j = m - 1;		// pattern의 맨 마지막
 		}
-	} while (i <= n - 1);
+	} while (i <= n - 1);		// text 길이보다 길어지기 전까지 반복
 	return -1;
 }
 
